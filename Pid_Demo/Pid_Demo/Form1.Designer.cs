6ca8349c6fce164fa1d562a1f9504ec8f3eb6dd0
AngusLiuYi <154724136@qq.com>
2026-06-12 07:21:16 +0800

    功能开发中

--- Pid_Demo/Pid_Demo/Form1.Designer.cs
+++ Pid_Demo/Pid_Demo/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            AntdUI.SliderMarkItem sliderMarkItem6 = new AntdUI.SliderMarkItem();
+            AntdUI.SliderMarkItem sliderMarkItem7 = new AntdUI.SliderMarkItem();
+            AntdUI.SliderMarkItem sliderMarkItem8 = new AntdUI.SliderMarkItem();
+            AntdUI.SliderMarkItem sliderMarkItem9 = new AntdUI.SliderMarkItem();
+            AntdUI.SliderMarkItem sliderMarkItem10 = new AntdUI.SliderMarkItem();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            Plot_M = new ScottPlot.WinForms.FormsPlot();
+            panel3 = new Panel();
+            slider8 = new AntdUI.Slider();
+            BtnStart = new AntdUI.Button();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(401, 744);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(Plot_M);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(401, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1077, 509);
+            panel2.TabIndex = 1;
+            // 
+            // Plot_M
+            // 
+            Plot_M.Dock = DockStyle.Fill;
+            Plot_M.Location = new Point(0, 0);
+            Plot_M.Margin = new Padding(5);
+            Plot_M.Name = "Plot_M";
+            Plot_M.Size = new Size(1077, 509);
+            Plot_M.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(BtnStart);
+            panel3.Controls.Add(slider8);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(401, 509);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1077, 235);
+            panel3.TabIndex = 2;
+            // 
+            // slider8
+            // 
+            slider8.Align = AntdUI.TAlignMini.Bottom;
+            slider8.Dock = DockStyle.Right;
+            slider8.Location = new Point(1023, 0);
+            sliderMarkItem6.Text = "-100";
+            sliderMarkItem6.Value = -100;
+            sliderMarkItem7.Text = "-50";
+            sliderMarkItem7.Value = -50;
+            sliderMarkItem8.Text = "0";
+            sliderMarkItem9.Text = "50";
+            sliderMarkItem9.Value = 50;
+            sliderMarkItem10.Text = "100";
+            sliderMarkItem10.Value = 100;
+            slider8.Marks.Add(sliderMarkItem6);
+            slider8.Marks.Add(sliderMarkItem7);
+            slider8.Marks.Add(sliderMarkItem8);
+            slider8.Marks.Add(sliderMarkItem9);
+            slider8.Marks.Add(sliderMarkItem10);
+            slider8.MinValue = -100;
+            slider8.Name = "slider8";
+            slider8.Padding = new Padding(0, 0, 34, 0);
+            slider8.Size = new Size(54, 235);
+            slider8.TabIndex = 2;
+            slider8.ValueChanged += slider8_ValueChanged;
+            slider8.MouseUp += slider8_MouseUp;
+            // 
+            // BtnStart
+            // 
+            BtnStart.AutoSizeMode = AntdUI.TAutoSize.Auto;
+            BtnStart.BackExtend = "135, #6253E1, #04BEFE";
+            BtnStart.IconSvg = "PlayCircleOutlined";
+            BtnStart.Location = new Point(889, 6);
+            BtnStart.Name = "BtnStart";
+            BtnStart.Size = new Size(128, 52);
+            BtnStart.TabIndex = 7;
+            BtnStart.Text = "启动跟随";
+            BtnStart.Type = AntdUI.TTypeMini.Primary;
+            BtnStart.Click += BtnStart_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1478, 744);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private ScottPlot.WinForms.FormsPlot Plot_M;
+        private AntdUI.Slider slider8;
+        private AntdUI.Button BtnStart;
     }
 }
--- Pid_Demo/Pid_Demo/Form1.cs
+++ Pid_Demo/Pid_Demo/Form1.cs
@@ -1,3 +1,8 @@
+using AntdUI;
+using AngusTools;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
 namespace Pid_Demo
 {
     public partial class Form1 : Form
@@ -5,6 +10,77 @@ namespace Pid_Demo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private double SetPosition { get; set; } = 0.0;
+        private int SetVelocity { get; set; } = 0;
+
+        private List<double> dtList = [];
+        private List<double> SetPosList = [];
+
+        private bool Processing { get; set; } = false;
+
+        private Task TaskProcess;
+
+        private void slider8_ValueChanged(object sender, IntEventArgs e) => SetVelocity = e.Value;
+
+        private void slider8_MouseUp(object sender, MouseEventArgs e) => slider8.Value = SetVelocity = 0;
+
+        private async void BtnStart_Click(object sender, EventArgs e)
+        {
+            AntdUI.Button btn = sender as AntdUI.Button ?? new AntdUI.Button();
+            btn.Loading= true;
+            if (btn.Text=="Æô¶¯¸úËæ")
+            {
+                btn.Text = "Í£Ö¹¸úËæ";
+                btn.IconSvg = "PauseCircleOutlined";
+                Processing = true;
+                TaskProcess = Task.Run(() => Process());
+            }
+            else
+            {
+                btn.Text = "Æô¶¯¸úËæ";
+                btn.IconSvg = "PlayCircleOutlined";
+                Processing = false;
+                await TaskProcess;
+            }
+            btn.Loading = false;
+        }
+
+        private void Process()
+        {
+            DateTime dt20ms = DateTime.Now, dt200ms = DateTime.Now;
+            Plot_M.Plot.Clear();
+            Plot_M.Plot.Add.ScatterLine(dtList, SetPosList);
+            while (Processing)
+            {
+                DateTime dtNow = DateTime.Now;
+                if ((dtNow - dt20ms).TotalMilliseconds>=20)
+                {
+                    dtList.Add(dtNow.TimeOfDay.TotalMilliseconds);
+
+                    //dtList.Add(dt20ms.TimeOfDay.TotalSeconds);
+                    SetPosList.Add(SetPosition += SetVelocity);
+                    if (dtList.Count>8000)
+                    {
+                        dtList.RemoveAt(0);
+                        SetPosList.RemoveAt(0);
+                    }
+                    dt20ms = dtNow;
+                }
+                if ((dtNow - dt200ms).Microseconds >= 200)
+                {
+                    Plot_M.Invoke(() =>
+                    {
+                        Plot_M.Plot.Axes.AutoScale();
+                        Plot_M.Refresh();
+                    });
+                    dt200ms = dtNow;
+                }
+
+                    Application.DoEvents();
+                Thread.Sleep(0);
+            }
         }
     }
 }
